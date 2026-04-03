27e75a7805c199b3d82cd9d35397792a7e813efb
Yash Mane <141533114+Ayankouji@users.noreply.github.com>
2026-04-03 20:12:32 +0530

    class & object basics

---
+++ oops1.py
@@ -0,0 +1,24 @@
+# Initiate a class
+class employee:
+   #Special method/magic method/dunder method - constructor
+   def __init__(self):
+      print("Started executing attribute/data")
+      self.id = 123
+      self.salary = 18000
+      self.designation = "SDE"
+      print("attribute/data have been initiated")
+
+   def travel(self,destination):
+      print("This Travel method was called manually")
+      print("The employee is travelling to",destination)
+
+# Create an object/instance of the class
+
+sam = employee()
+# printing the attribute/data of the class
+# print(sam.id)
+
+#calling a method
+# sam.travel("Bangalore")
+# when u create function under the class, it is called method. when u create variable under the class, it is called attribute. when u create object of the class, it is called instance. when u create function under the class, it is called method. when u create variable under the class, it is called attribute. when u create object of the class, it is called instance. when u create function under the class, it is called method. when u create variable under the class, it is called attribute. when u create object of the class, it is called instance. when u create function under the class, it is called method. when u create variable under the class, it is called attribute. when u create object of the class, it is called instance. when u create function under the class, it is called method. when u create variable under the class, it is called attribute. when u create object of the class, it is called instance.  
+
