8745e95e949cdad7a75baf6f4a6a30743104bc7e
Yash Mane <141533114+Ayankouji@users.noreply.github.com>
2026-04-05 01:31:49 +0530

    Encapsulation, getter setter and static method covered

--- rough.py
+++ rough.py
@@ -1,3 +1,32 @@
 from oops_proj import chatbook
 #Modular coding
 user1 = chatbook()
+print(user1.id)
+
+
+#using static method directly from class rather than obj
+chatbook.set_id(10)
+
+user2 = chatbook()
+print(user2.id)
+
+user3 = chatbook()
+print(user3.id)
+
+# print()
+# user2 = chatbook()
+# print(user2.id)
+
+# user3 = chatbook()
+# print(user3.id)
+
+
+
+# print(user1._chatbook__name) -> To accesss private variable outside the class
+
+
+#Getter & Setter method
+# print(user1.get_name())
+# user1.set_name("Agent")
+# print(user1.get_name())
+
